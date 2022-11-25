db0d7972919a90a1cda7aa266a8d5e70b0273b7b
Andreza Campbell <campbell.andreza@gmail.com>
2022-11-25 11:34:41 -0500

    using useEffect for stock

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -3,19 +3,26 @@ import { useEffect } from 'react';
 import './ItemCount.css';
 
 function ItemCount() {
-  const stock = 5;
+  const [stock, setStock] = useState(5);
   const [click, setClick] = useState(0);
 
   useEffect(() => {
-    if (click >= stock) {
+    if (stock === 0) {
       const greater = document.getElementById('btnAddCart');
       greater.classList.add('disable');
+    }
+  }, [click, stock]);
+
+  useEffect(() => {
+    if (stock > 0 && click !== 0) {
+      setStock(stock - 1);
     }
   }, [click]);
 
   return (
     <div>
       <p>Number of clicks: {click}</p>
+      <p>Stock: {stock}</p>
       <button id="btnAddCart" onClick={() => setClick(click + 1)}>
         Add to cart
       </button>
